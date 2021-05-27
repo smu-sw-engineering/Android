e07b168efb4622439cf374bca073e89e4b559512
sunjungAn <dkstjswjd@naver.com>
2021-05-27 15:40:10 +0900

    button

--- app/src/main/java/com/example/sw_engineering/customer/cusBookCamp.java
+++ app/src/main/java/com/example/sw_engineering/customer/cusBookCamp.java
@@ -1,6 +1,7 @@
 package com.example.sw_engineering.customer;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.sw_engineering.R;
 
 public class cusBookCamp extends AppCompatActivity {
+    private Button book_button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cus_book_camp);
+
+        book_button = (Button)findViewById((R.id.book));
     }
 
 }
