b6455f3e72221f98d3405abdd6add57183d029c6
yoonho <yoonho0922@naver.com>
2021-05-27 16:36:21 +0900

    feat: common 패키지 액티비티 추가

---
+++ app/src/main/java/com/example/sw_engineering/common/ComInfoApp.java
@@ -0,0 +1,16 @@
+package com.example.sw_engineering.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sw_engineering.R;
+
+public class ComInfoApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_info_app);
+    }
+}
---
+++ app/src/main/java/com/example/sw_engineering/common/ComUserInfoMod.java
@@ -0,0 +1,16 @@
+package com.example.sw_engineering.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sw_engineering.R;
+
+public class ComUserInfoMod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_user_info_mod);
+    }
+}
