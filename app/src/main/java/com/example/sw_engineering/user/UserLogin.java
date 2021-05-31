972c772feea3798d51d32ff69c6f50beb85418f6
hyeji1221 <lhg2615@naver.com>
2021-05-31 14:43:09 +0900

    feat: login.xml

--- app/src/main/java/com/example/sw_engineering/user/UserLogin.java
+++ app/src/main/java/com/example/sw_engineering/user/UserLogin.java
@@ -3,14 +3,24 @@ package com.example.sw_engineering.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sw_engineering.R;
 
 public class UserLogin extends AppCompatActivity {
-
+    private Button find_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        find_id = findViewById(R.id.findB);
+
+        find_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
