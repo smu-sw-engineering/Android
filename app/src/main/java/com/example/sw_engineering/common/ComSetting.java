9d880c085b3ab6db209c333f3997a3d2cf9651cd
sunjungAn <dkstjswjd@naver.com>
2021-06-03 16:14:40 +0900

    comsetting완료

--- app/src/main/java/com/example/sw_engineering/common/ComSetting.java
+++ app/src/main/java/com/example/sw_engineering/common/ComSetting.java
@@ -1,16 +1,84 @@
 package com.example.sw_engineering.common;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sw_engineering.R;
+import com.example.sw_engineering.user.UserLogin;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ComSetting extends AppCompatActivity {
-
+    TextView username;
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com_setting);
+        username = findViewById(R.id.username);
+        findViewById(R.id.logout).setOnClickListener(onClickListener);
+        findViewById(R.id.btn_info_mod).setOnClickListener(onClickListener);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("UserInfo").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                name = document.getString("name");
+                username.setText(name);
+            }
+        });
+
+
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) { //클릭된 위젯의 id를 이용
+            switch (v.getId()) {
+                case R.id.logout:
+                    SignOut();
+                    startLoginActivity();
+                    break;
+                case R.id.btn_info_mod:
+                    startComUserInfoMod();
+
+            }
+        }
+    };
+    private void SignOut() {
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+            startToast("로그아웃 했습니다.");
+        }
+    }
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
+    }
+    private void startComUserInfoMod() {
+        Intent intent = new Intent(this, ComUserInfoMod.class);
+        startActivity(intent);
     }
 }
