40c1f48aa82b18032bfb75a42630a16e49816a75
hyeji1221 <lhg2615@naver.com>
2021-06-03 14:46:33 +0900

    feat: cusSearch추가

---
+++ app/src/main/java/com/example/sw_engineering/customer/cusSearch.java
@@ -0,0 +1,16 @@
+package com.example.sw_engineering.customer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sw_engineering.R;
+
+public class cusSearch extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cus_search);
+    }
+}
